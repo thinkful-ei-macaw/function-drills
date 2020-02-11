27ffd25ca33228ec77a4bd0aa0c39b9513734f4b
Dasha Akhten <dasha.akhten@gmail.com>
2020-02-10 16:08:18 -0800

    added functions

--- function-drills.js
+++ function-drills.js
@@ -3,3 +3,9 @@ function greeting (){
     console.log ('Hi my name is Dasha, and I am 35 years old');
     }
     greeting ();
+
+function createGreeting (){
+    return = 'heelo my name is Dasha'
+};
+const greeting1=createGreeting();
+console.log(greeting1);
